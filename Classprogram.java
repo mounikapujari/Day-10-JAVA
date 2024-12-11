d8224119fe2853214c2d275c56d69bfd5c39181d
mounikapujari <mounikapujari2005@gmail.com>
2024-12-11 11:19:00 +0530

    Add files via upload

---
+++ Classprogram.java
@@ -0,0 +1,11 @@
+class a{
+    int a1=10;
+    int a2=20;
+}
+class Classprogram{
+    public static void main(String args[]){
+        a r=new a();
+        System.out.println(r.a1);
+        System.out.println(r.a2);
+    }
+}
