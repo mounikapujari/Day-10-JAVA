f30696b51d0cc52ef931f6a88e08cba9e9aa3a04
mounikapujari <mounikapujari2005@gmail.com>
2024-12-11 14:51:30 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,15 @@
+public class Fibonacci {
+    public static int fib(int n)
+    {
+        if(n==0 || n==1)
+        {
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+    public static void main(String[] args) {
+        int res=fib(10);
+        System.out.println(res);
+    }
+    
+}
