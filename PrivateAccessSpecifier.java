354ea170716867c71ae8baf33c0d66bcdd18d1ec
mounikapujari <mounikapujari2005@gmail.com>
2024-12-11 11:20:49 +0530

    Add files via upload

---
+++ PrivateAccessSpecifier.java
@@ -0,0 +1,9 @@
+public class PrivateAccessSpecifier {
+    private static void s() {
+       System.out.println("Hello Students");
+    }
+ 
+    public static void main(String[] args) {
+       s();
+    }
+}
