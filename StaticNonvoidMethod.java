ccefbcd0a5731da9cfb09b4a38709484602e2fd5
mounikapujari <mounikapujari2005@gmail.com>
2024-12-11 11:21:41 +0530

    Add files via upload

---
+++ StaticNonvoidMethod.java
@@ -0,0 +1,10 @@
+ class StaticNonvoidMethod{
+    public static String s()
+    {
+        return "Hello Mouni";
+    }
+    public static void main(String[] args) {
+        String res=s();
+        System.out.println(res);
+    }
+ }
