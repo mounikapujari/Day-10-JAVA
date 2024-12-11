0208c012bd121467878ebabb120f56dd568d7dc4
mounikapujari <mounikapujari2005@gmail.com>
2024-12-11 11:21:12 +0530

    Add files via upload

---
+++ ProtectedAccessSpecifier.java
@@ -0,0 +1,9 @@
+public class ProtectedAccessSpecifier {
+    protected static void s() {
+        System.out.println("Java programming");
+     }
+  
+     public static void main(String[] args) {
+        s();
+     }
+}
